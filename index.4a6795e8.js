629389a169f2badf790c0f9938921455d0ea3a4a
VasylynaBC <you@example.com>
2024-10-08 11:45:31 +0300

    make build

---
+++ index.4a6795e8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target;"IMG"===r.tagName&&(r=r.parentElement),t.src=r.href});
+//# sourceMappingURL=index.4a6795e8.js.map
